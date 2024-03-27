d04a2553c1f009e35253a53fdcfb41d7aee44675
Ikaika Lee <leeikaika4@gmail.com>
2024-03-27 12:46:59 -0500

    removed last @Query

--- inkparadise/src/main/java/com/catsaredope/inkparadise/Repositories/ReadingRepository.java
+++ inkparadise/src/main/java/com/catsaredope/inkparadise/Repositories/ReadingRepository.java
@@ -3,12 +3,10 @@ package com.catsaredope.inkparadise.Repositories;
 import com.catsaredope.inkparadise.Models.Reading;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReadingRepository extends JpaRepository<Reading, Long> {
-  @Query("select r from Reading r where r.userId = ?1")
   List<Reading> findByUserId(Long userId);
 
   List<Reading> findByUserIdAndMangaNameContaining(Long userId, String mangaName);
