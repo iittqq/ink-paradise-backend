7c943ad8c68a3a8023603e4afa6e95a40f5ae9ae
Ikaika Lee <leeikaika4@gmail.com>
2024-05-25 14:05:33 -0500

    removed redundant code from accountController

--- inkparadise/src/main/java/com/catsaredope/inkparadise/Models/LoginRequest.java
+++ inkparadise/src/main/java/com/catsaredope/inkparadise/Models/LoginRequest.java
@@ -5,8 +5,6 @@ public class LoginRequest {
   private String email;
   private String password;
 
-  public LoginRequest() {}
-
   public LoginRequest(String email, String password) {
     this.email = email;
     this.password = password;
